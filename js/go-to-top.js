cb6df7cb6ea38013822a9737d02a87f4b9219ecb
UCHINO Yuki <160313760+UCHINO-Yuki@users.noreply.github.com>
2025-04-06 03:14:29 +0900

    cat added

--- js/go-to-top.js
+++ js/go-to-top.js
@@ -1,6 +1,7 @@
 const pagetopBtn = document.querySelector('.to-pagetop');
+const cat = document.getElementById("scroll-image");
 
-toScrollTop = () => {
+function toScrollTop() {
   window.scroll({
     top: 0,
     behavior: "smooth"
@@ -15,5 +16,31 @@ const scrollEvents = () => {
   }
 }
 
-pagetopBtn.addEventListener('click', toScrollTop);
+pagetopBtn.addEventListener('click', () => {
+  cat.style.display = "flex";
+  toScrollTop();
+});
+
 window.addEventListener('scroll', scrollEvents);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const scrollImage = document.getElementById('scroll-image');
+  const scrollButton = document.getElementById('scroll-button');
+
+  function syncImageToScroll() {
+    const scrollTop = window.scrollY;
+    const scrollHeight = document.body.scrollHeight - window.innerHeight;
+    const percentage = scrollTop / scrollHeight;
+
+    const moveRange = window.innerHeight * 1.2;
+    const newtop = moveRange * percentage - 120;
+    scrollImage.style.top = `${newtop}px`;
+    if (newtop == -120) {
+      scrollImage.style.display = "none";
+    }
+  }
+
+  window.addEventListener('scroll', syncImageToScroll);
+
+  syncImageToScroll(); // 初期位置をセット
+});
